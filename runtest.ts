5bd5f82f98d7977784abd2fff43b11fc58bc6d86
Jupegarnica <juan@geekshubs.com>
2020-12-05 11:31:50 +0100

    test readme

--- runtest.ts
+++ runtest.ts
@@ -1,4 +1,5 @@
-import { writeYaml } from "./mod.ts";
+import { writeYaml } from 'https://deno.land/x/garn_yaml@0.2.1/mod.ts';
 
-const data = {a: 1, b: 2};
-await writeYaml('./write-test.yml', data);
+const yamlText = await writeYaml('./write-test.yml', { a: 1 }); // a: 1
+
+console.log(yamlText);
